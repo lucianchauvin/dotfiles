0db6080a749579312abc615f9198c67fb9b44d84
Lucian Chauvin <lucianchauvin@gmail.com>
2024-04-04 16:02:23 -0500

    more qute

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,4 +1,5 @@
 config.load_autoconfig()
 config.source('gruvbox.py')
 c.editor.command = ['alacritty', '-e', 'nvim', '{file}', '-c', 'normal {line}G{column0}l']
+c.fonts.default_size = "11pt"
 config.bind("<Escape>", "clear-messages")
