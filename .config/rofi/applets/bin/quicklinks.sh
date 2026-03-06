7797ef037f2e64a8db9ff82d840aac55596df41d
Lucian Chauvin <lucianchauvin@gmail.com>
2026-03-06 08:22:00 -0600

    comic shanns

--- .config/rofi/applets/bin/quicklinks.sh
+++ .config/rofi/applets/bin/quicklinks.sh
@@ -22,9 +22,9 @@ elif [[ ( "$theme" == *'type-2'* ) || ( "$theme" == *'type-4'* ) ]]; then
 fi
 
 if [[ ( "$theme" == *'type-1'* ) || ( "$theme" == *'type-5'* ) ]]; then
-	efonts="NotoSansM Nerd Font Mono 12.5"
+	efonts="ComicShannsMono Nerd Font Mono 14.05"
 else
-	efonts="JetBrains Mono Nerd Font 28"
+	efonts="ComicShannsMono Nerd Font Mono 14.05"
 fi
 
 # Options
